e62b003bb4c7a32d62a1c1d34481031b47654c50
Devon Zuegel <devonzuegel@gmail.com>
2020-12-22 18:58:45 -0800

    Get inline tag extraction working

--- src/timeline/core.cljs
+++ src/timeline/core.cljs
@@ -1,23 +1,19 @@
 (ns timeline.core
-  (:require [rum.core :as rum]))
+  (:require [rum.core :as rum])
+  (:require-macros [timeline.utils :refer [inline-resource]]))
 
+(def example-text (inline-resource "hopl-clojure.html"))
+
+(println)
 (enable-console-print!)
 
-(println "This text is printed from src/timeline/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello, world.", :counter 0}))
-
-(defn inc-counter [current-app-state] (update-in current-app-state [:counter] inc)) ;; No need to deref it
+(def inline-date-tags (array-seq (.getElementsByClassName js/document "timeline-item")))
+(def years (for [d inline-date-tags] (.-innerText d)))
 
 (rum/defc hello-world < rum/reactive
-  []
-  (let [state (rum/react app-state)] ;; Register a listener to tell this component to react to the state.
-    [:div
-     [:h1 (:text state)] ;; Dereference the box; "open the box and give me the value"
-     [:p "Counter: " (:counter state)]
-     [:button {:on-click (fn [e] (swap! app-state inc-counter)) :class "btn"} "Click me!"]]))
+  ([]
+   [:div
+    [:div {:dangerouslySetInnerHTML {:__html example-text}}]]))
 
-(rum/mount (hello-world)
-           (. js/document (getElementById "app"))) ;; Here's how you use JS's dot operator
+;; Here's how you use JS's dot operator
+(rum/mount (hello-world) (. js/document (getElementById "app")))
