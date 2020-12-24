73abb0725095e0b474467e5c79c345a7632a8a10
Devon Zuegel <devonzuegel@gmail.com>
2020-12-23 19:39:12 -0800

    Extract min- and max-year

--- src/timeline/core.cljs
+++ src/timeline/core.cljs
@@ -10,29 +10,26 @@
 ; (def inline-date-tags (array-seq (.getElementsByClassName js/document "timeline-item")))
 ; (def years (for [d inline-date-tags] (int (.-innerText d)))) ; TODO: Put back
 (def years [2000 2001 2003 2004 2010])
-(def min-year (apply min years))
-(def max-year (apply max years))
 
-(defn get-percent [y] (* 100  (divide (- y min-year) (- max-year min-year))))
-(defn get-adjusted-percent [y] (+ 2 (* (get-percent y) .9))) ; Ajust so the timeline doesn't take up the entire screen
-(print "min-year:" min-year)
-(print "min-year:" min-year)
+(defn get-percent [y -min-year -max-year]
+  (* 100  (divide (- y -min-year) (- -max-year -min-year))))
 
+; Ajust so the timeline only takes 90% of the screen
+(defn get-adjusted-percent [y -min-year -max-year]
+  (+ 2 (* (get-percent y -min-year -max-year) .9)))
 
 (defn render-year [y]
-  [:span
-   {:className "point"
-    :style {:left (str (get-adjusted-percent y) "vw")}}
-   y])
+  (let [min-year (apply min years)
+        max-year (apply max years)]
+    [:span
+     {:className "point"
+      :style {:left (str (get-adjusted-percent y min-year max-year) "vw")}}
+     y]))
 
 (rum/defc hello-world < rum/reactive
   ([]
    [:div
-    [:div {:class "timeline"}
-    ;  [:span {:class "point"} min-year]
-     (map render-year years)
-    ;  [:span {:class "point"} max-year]
-     ]
+    [:div {:class "timeline"} (map render-year years)]
     ; [:button {:onClick #(js/alert "hello")}  "Click me"]
     [:div {:dangerouslySetInnerHTML {:__html example-text}}]]))
 
@@ -43,9 +40,9 @@
 
 ; Tests
 (if (not (and
-          (= 100 (get-percent 2010))
-          (= 92 (get-adjusted-percent 2010)) ; = (100 * .9) + 2
-          (= 11 (get-adjusted-percent 2001)) ; = (10 * .9) + 2
-          (= 2 (get-adjusted-percent 2000))) ; = (0 * .9) + 2
+          (= 100 (get-percent 2010 2000 2010))
+          (= 92 (get-adjusted-percent 2010 2000 2010)) ; = (100 * .9) + 2
+          (= 11 (get-adjusted-percent 2001 2000 2010)) ; = (10 * .9) + 2
+          (= 2 (get-adjusted-percent 2000 2000 2010))) ; = (0 * .9) + 2
 ) ; Add new tests here
   (js/alert "Tests are failing!"))
