a48dbbd91e415bfd230261a345f39a6a09cb4672
Devon Zuegel <devonzuegel@gmail.com>
2021-01-07 12:43:41 -0800

    Got Recogito to load 1 annotation!

--- src/timeline/core.cljs
+++ src/timeline/core.cljs
@@ -170,20 +170,20 @@
     ;; (. annotator on "createAnnotation" #(console.log "createAnnotation!"))
     ;; (console.log (first (first annotations)))
     ;; (print (first annotations))
-    ;; ;; (. annotator addAnnotation #js (first (first annotations)))
+    ;; (. annotator addAnnotation (first (first annotations)))
     ;; (. annotator loadAnnotations "example-annotations.json")
     ;; (. annotator loadAnnotations "https://jsonbox.io/box_bd8115672b6df30ba312/3fed311908235266ff7c")
-    (. annotator addAnnotation {:type "Annotation"
-                                :target {:selector [{:type "TextQuoteSelector"
-                                                     :exact "October of 1990"}
-                                                    {:type "TextPositionSelector"
-                                                     :start 1881
-                                                     :end 1896}]}
-                                :id "#5f41fdde-0a39-44ac-a816-08ae60402709"
-                                :body [{:type "TextualBody"
-                                        :purpose "time-annotation"
-                                        :value "1990-10-01T04:00:00.000Z"
-                                        :timeSelectionType "month"}]})
+    (. annotator addAnnotation (clj->js {:type "Annotation"
+                                         :target {:selector [{:type "TextQuoteSelector"
+                                                              :exact "October of 1990"}
+                                                             {:type "TextPositionSelector"
+                                                              :start 1881
+                                                              :end 1896}]}
+                                         :id "#5f41fdde-0a39-44ac-a816-08ae60402709"
+                                         :body [{:type "TextualBody"
+                                                 :purpose "time-annotation"
+                                                 :value "1990-10-01T04:00:00.000Z"
+                                                 :timeSelectionType "month"}]}))
     ;; (babys-first-macro HtmlSanitizer)
     ;; (babys-first-macro htmlSanitizer)
     (babys-first-macro annotator)))
